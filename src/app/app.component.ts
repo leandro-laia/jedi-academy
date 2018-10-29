c5d08e3f105be876d40b8c711032a800a7aa41f1
Leandro Laia <misterlaia@gmail.com>
2018-10-29 01:29:18 -0300

    jedi-acedemy inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jda-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jedi-academy';
+  title = 'The Force be with you!';
 }
