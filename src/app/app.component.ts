454b50a053f18a6ece3df037ca91511639877ea7
Leandro Oliveira Laia <leandrolaia@tjrj.jus.br>
2018-10-30 18:30:56 -0300

    uso de ngIf ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './student/student.model';
 
 @Component({
   selector: 'jda-root',
@@ -6,8 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-    luke = {name : 'Luke Skywalker', isJedi : true, temple : 'Coruscant' }
-    han = {name : 'Han Solo', isJedi : false}
-    leia = {name : 'Leia', isJedi : false }
+    students : Student[] =
+    [
+       {name : 'Luke Skywalker', isJedi : true, temple : 'Coruscant' },
+       {name : 'Han Solo', isJedi : false},
+       {name : 'Leia', isJedi : false }
+    ]
+
 
 }
